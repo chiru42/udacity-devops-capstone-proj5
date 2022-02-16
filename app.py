e0a4724c70b6a4c75c8fc95997cfe25d47ed54f1
chiru42 <46132793+chiru42@users.noreply.github.com>
2022-02-16 10:11:53 +0900

    added app code

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask,render_template,request,jsonify
+# from sklearn.preprocessing import StandardScaler
+import json
+import joblib
+import logging
+ 
+app = Flask(__name__)
+
+def scale(payload):
+    """Scales Payload"""
+    
+    LOG.info(f"Scaling Payload: \n{payload}")
+    scaler = StandardScaler().fit(payload.astype(float))
+    scaled_adhoc_predict = scaler.transform(payload.astype(float))
+    return scaled_adhoc_predict
+ 
+@app.route('/form')
+def form():
+    return render_template('inputform.html')
+ 
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        print("payload: ",form_data.get('param1'))
+        payload = {  
+        "CHAS":{  
+            "0":form_data.get('param1')
+        },
+        "RM":{  
+            "0":form_data.get('param2')
+        },
+        "TAX":{  
+            "0":form_data.get('param3')
+        },
+        "PTRATIO":{  
+            "0":form_data.get('param4')
+        },
+        "B":{  
+            "0":form_data.get('param5')
+        },
+        "LSTAT":{  
+            "0":form_data.get('param6')
+        }
+        }
+        json_payload = json.dumps(payload)
+        print("json payload: ",json_payload)
+
+        # # Logging the input payload
+        # # json_payload = request.json
+        # # LOG.info(f"JSON payload: \n{json_payload}")
+        # inference_payload = pd.DataFrame(json_payload)
+        # # LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
+        # # scale the input
+        # scaled_payload = scale(inference_payload)
+        # # get an output prediction from the pretrained model, clf
+        # prediction = list(clf.predict(scaled_payload))
+        # print("Prediction: ",prediction)
+        # # TO DO:  Log the output prediction value
+        # # LOG.info(f"Prediction output DataFrame: \n{prediction}")
+        # # return jsonify({'prediction': prediction})
+
+        return render_template('data.html',form_data = jsonify({'prediction: ',10}))
+ 
+if __name__ == "__main__":
+    # load pretrained model as clf
+    # clf = joblib.load("./model_data/boston_housing_prediction.joblib")
+    # app.run(host='localhost', port=5000, debug=True) # specify port=80
+    app.run(host='localhost', port=5000)
